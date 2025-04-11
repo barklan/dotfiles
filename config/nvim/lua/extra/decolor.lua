e630e79fae8ccc0b9e5f843fd315647eee5beeb6
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-04-12 00:30:28 +0300

    fixup!

--- config/nvim/lua/extra/decolor.lua
+++ config/nvim/lua/extra/decolor.lua
@@ -5,26 +5,28 @@ local M = {}
 -- @type.definition.go - this works
 -- @type.definition    - this does not
 M.golang = function()
-    vim.api.nvim_set_hl(0, "@keyword.type.go", { link = "@none", force = true }) -- anonymous struct
+    -- vim.api.nvim_set_hl(0, "@keyword.type.go", { link = "@none", force = true }) -- anonymous struct also go here
 
-    -- vim.api.nvim_set_hl(0, "@string.go", { link = "Special", force = true })
-    vim.api.nvim_set_hl(0, "@string.go", { fg = "#7e92eb", force = true })
+    vim.api.nvim_set_hl(0, "@string.go", { fg = "#cfd5c9", force = true })
+    -- vim.api.nvim_set_hl(0, "@string.go", { fg = "#7e92eb", force = true })
     vim.api.nvim_set_hl(0, "@character.printf", { link = "@string.go", force = true })
 
     vim.api.nvim_set_hl(0, "@number.go", { link = "@none", force = true })
+    vim.api.nvim_set_hl(0, "@boolean.go", { link = "@none", force = true })
 
     -- NOTE: USEFULL 1st level
-    vim.api.nvim_set_hl(0, "@type.definition.go", { link = "@none", force = true })
-    vim.api.nvim_set_hl(0, "@function.go", { link = "@none", force = true })
-    vim.api.nvim_set_hl(0, "@function.method.go", { link = "@none", force = true })
+    -- vim.api.nvim_set_hl(0, "@type.definition.go", { link = "@none", force = true })
+    -- vim.api.nvim_set_hl(0, "@function.go", { link = "@none", force = true })
+    -- vim.api.nvim_set_hl(0, "@function.method.go", { link = "@none", force = true })
 
     -- NOTE: USEFULL 2nd level (functions)
-    vim.api.nvim_set_hl(0, "@function.method.call.go", { link = "@none", force = true })
-    vim.api.nvim_set_hl(0, "@function.call.go", { link = "@none", force = true })
+    -- vim.api.nvim_set_hl(0, "@function.method.call.go", { link = "@none", force = true })
+    -- vim.api.nvim_set_hl(0, "@function.call.go", { link = "@none", force = true })
 
     -- NOTE: USEFULL 3rd level (types)
-    vim.api.nvim_set_hl(0, "@type.go", { link = "@none", force = true })
-    vim.api.nvim_set_hl(0, "@type.builtin.go", { link = "@none", force = true })
+    -- vim.api.nvim_set_hl(0, "@type.go", { link = "@none", force = true })
+    -- vim.api.nvim_set_hl(0, "@type.builtin.go", { link = "@none", force = true })
+    -- vim.api.nvim_set_hl(0, "@constant.builtin.go", { link = "@none", force = true })
 
     -- NOTE: the rest
     vim.api.nvim_set_hl(0, "@module.go", { link = "@none", force = true })
@@ -32,13 +34,11 @@ M.golang = function()
 
     vim.api.nvim_set_hl(0, "@property.go", { link = "@none", force = true })
     vim.api.nvim_set_hl(0, "@constant.go", { link = "@none", force = true })
-    vim.api.nvim_set_hl(0, "@variable.go", { link = "@none", force = true })
-    vim.api.nvim_set_hl(0, "@function.builtin.go", { link = "@none", force = true })
+    -- vim.api.nvim_set_hl(0, "@variable.go", { link = "@none", force = true })
+    -- vim.api.nvim_set_hl(0, "@function.builtin.go", { link = "@none", force = true })
     vim.api.nvim_set_hl(0, "@punctuation.delimiter.go", { link = "@none", force = true })
-    vim.api.nvim_set_hl(0, "@constant.builtin.go", { link = "@none", force = true })
     vim.api.nvim_set_hl(0, "@operator.go", { link = "@none", force = true })
     vim.api.nvim_set_hl(0, "@variable.parameter.go", { link = "@none", force = true })
-    vim.api.nvim_set_hl(0, "@boolean.go", { link = "@none", force = true })
 end
 
 return M
