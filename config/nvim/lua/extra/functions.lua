e817032af2e31e21cbca0b8ad5506d0f0df4f438
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-04-13 03:50:42 +0300

    fixup!

--- config/nvim/lua/extra/functions.lua
+++ config/nvim/lua/extra/functions.lua
@@ -22,7 +22,7 @@ M.get_directories = function()
 end
 
 M.rg_all_cwd_lines_important_args = "--hidden --ignore --max-columns=500 --max-filesize=5M"
-M.rg_all_cwd_lines_limit = 1000000
+M.rg_all_cwd_lines_limit = 200000
 
 M.rg_all_cwd_lines = function()
     local results = {}
