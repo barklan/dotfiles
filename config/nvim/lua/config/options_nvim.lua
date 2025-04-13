7a3d2e817d20077f3fbef9810fda1dc3af6fedb3
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-04-13 03:12:06 +0300

    fixup!

--- config/nvim/lua/config/options_nvim.lua
+++ config/nvim/lua/config/options_nvim.lua
@@ -127,27 +127,5 @@ vim.defer_fn(function()
     vim.cmd([[set title titlestring=]] .. title)
 end, 20)
 
--- https://github.com/kovidgoyal/kitty/issues/108
-vim.cmd([[
-" vim hardcodes background color erase even if the terminfo file does
-" not contain bce (not to mention that libvte based terminals
-" incorrectly contain bce in their terminfo files). This causes
-" incorrect background rendering when using a color theme with a
-" background color.
-let &t_ut=''
-]])
-
--- vim.cmd([[
--- if has('gui_running') || has('nvim')
---     hi Normal 		guifg=#f6f3e8 guibg=#1a1b26
--- else
---     " Set the terminal default background and foreground colors, thereby
---     " improving performance by not needing to set these colors on empty cells.
---     hi Normal guifg=NONE guibg=NONE ctermfg=NONE ctermbg=NONE
---     let &t_ti = &t_ti . "\033]10;#f6f3e8\007\033]11;#242424\007"
---     let &t_te = &t_te . "\033]110\007\033]111\007"
--- endif
--- ]])
-
 -- TODO: move somewhere else
 vim.cmd("cabbrev g !git")
