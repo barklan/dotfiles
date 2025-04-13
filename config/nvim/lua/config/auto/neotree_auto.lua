efdb3fe0c1783492dd7a5a11dc845755e93f37ea
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-04-13 03:16:09 +0300

    fixup!

--- config/nvim/lua/config/auto/neotree_auto.lua
+++ config/nvim/lua/config/auto/neotree_auto.lua
@@ -5,14 +5,20 @@ vim.api.nvim_create_autocmd("VimEnter", { -- Changed from BufReadPre
     group = "neotree_auto",
     once = true,
     callback = function()
+        if IsGitEditor() then
+            vim.g.neotree_opened = true
+            vim.cmd(":Neotree show source=git_status position=left") -- source can be "last"
+        end
+        -- NOTE: mb find a way to replace existing buffer to load even faster (also look in auto-session)
+
         local num_args = vim.fn.argc()
-        if num_args == 0 and not IsScrollbackPager() then
+        if num_args == 0 then
             vim.schedule(function()
                 if not vim.g.neotree_opened then
                     -- Neotree position=current
                     -- NOTE: mb find a way to replace existing buffer to load even faster (also look in auto-session)
-                    vim.cmd("Neotree show")
                     vim.g.neotree_opened = true
+                    vim.cmd("Neotree show")
                 end
             end)
         end
