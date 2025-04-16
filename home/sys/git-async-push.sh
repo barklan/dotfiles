1ce191888e9b36d546a38b39cd05f23d828a591d
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-04-16 19:50:20 +0300

    fixup!

---
+++ home/sys/git-async-push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+CURRENT_BRANCH=$(git branch --show-current)
+systemd-run --same-dir --collect --user bash -c "git push origin ${CURRENT_BRANCH}"
