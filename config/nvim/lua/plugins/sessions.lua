64051418912ab67715d4db2df713d37b59338e4d
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-04-18 19:27:37 +0300

    fixup!

--- config/nvim/lua/plugins/sessions.lua
+++ config/nvim/lua/plugins/sessions.lua
@@ -3,7 +3,6 @@ return {
         "folke/persistence.nvim",
         -- event = "BufReadPre", -- this will only start session saving when an actual file was opened
         lazy = false,
-        priority = 900,
         cond = function()
             if InVSCode() then
                 return false
