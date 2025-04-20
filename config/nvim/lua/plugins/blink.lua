ed5e13405d91cf08bcb598a3780e4ca67efd89f6
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-04-20 16:52:44 +0300

    fixup!

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -102,7 +102,7 @@ return {
                     },
                     documentation = {
                         auto_show = true,
-                        auto_show_delay_ms = 200,
+                        auto_show_delay_ms = 400,
                     },
                     menu = {
                         draw = {
