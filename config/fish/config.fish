56c5a5bd25696cff8bbcda3692a1bfb0d32412cf
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-05-09 13:40:14 +0300

    fixup!

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -96,7 +96,6 @@ if status is-interactive
     set -gx FORGIT_CHECKOUT_BRANCH_BRANCH_GIT_OPTS '--sort=-committerdate'
     set -gx FORGIT_REBASE_GIT_OPTS -v
     set -gx GOGC 500
-
 end
 
 function __on_nvim_exit --on-event nvim_exit
