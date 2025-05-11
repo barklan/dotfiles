5fb7b806273cd8582e5c646a14c47975ba0f4481
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-05-11 13:11:28 +0300

    fixup!

--- home/sys/git_reset_from_upstream.fish
+++ home/sys/git_reset_from_upstream.fish
@@ -1,6 +1,6 @@
 set -l current $(git branch --show-current)
-set -l current_push $(git rev-parse --abbrev-ref @{push})
+set -l current_push $(git rev-parse --abbrev-ref @{u})
 
 if yes_or_no "Are you sure you want to reset $current from $current_push?" false
-    git fetch origin && git reset --hard @{push}
+    git fetch origin && git reset --hard @{u}
 end
