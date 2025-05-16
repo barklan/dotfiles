cb74b899087c689c67f90bebde22f751f6ad35c7
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-05-16 19:44:53 +0300

    fixup!

--- home/sys/git-notify-behind.sh
+++ home/sys/git-notify-behind.sh
@@ -5,18 +5,24 @@ IFS=$'\n\t'
 export BRANCH="$(git default-branch-name)"
 export CURRENT="$(git branch --show-current)"
 
+BRANCH_EXISTS=$(git branch --list "origin/${CURRENT}" | wc -l)
+
+if [ "${BRANCH_EXISTS}" == "0" ]; then
+    exit 0
+fi
+
 if [ "${BRANCH}" == "${CURRENT}" ]; then
-	# All personal repos are on main, don't notify
-	if [ "${CURRENT}" == "main" ]; then
-		exit 0
-	fi
-	notify-send "Committing to ${CURRENT}!"
-	exit 0
+    # All personal repos are on main, don't notify
+    if [ "${CURRENT}" == "main" ]; then
+        exit 0
+    fi
+    notify-send "Committing to ${CURRENT}!"
+    exit 0
 fi
 
 retVal=$?
 if [ $retVal -eq 124 ]; then
-	notify-send "fuck"
+    notify-send "fuck"
 fi
 
 export LEFT_RIGHT=$(git rev-list --left-right --count "origin/${BRANCH}"..."${CURRENT}")
@@ -25,5 +31,5 @@ export LEFT_RIGHT_ARR=($LEFT_RIGHT)
 export BEHIND="${LEFT_RIGHT_ARR[0]}"
 
 if [ "${BEHIND}" != "0" ]; then
-	notify-send "${CURRENT} is behind origin/${BRANCH} by ${BEHIND} commits!"
+    notify-send "${CURRENT} is behind origin/${BRANCH} by ${BEHIND} commits!"
 fi
