89385942ff798394e0e77cf8ee6b70e9e6f828ed
Gleb Buzin <qufiwefefwoyn@gmail.com>
2025-05-30 17:51:13 +0300

    fixup!

--- home/sys/git-status.sh
+++ home/sys/git-status.sh
@@ -10,21 +10,21 @@ printf "Last commit: %s\n" "$(git log -1 --pretty=format:'%h %s [%an, %ar]')"
 default=$(git default-branch-name)
 current=$(git branch --show-current)
 
-default_ab=$(git rev-list --left-right --count @..."$default")
+default_ab=$(git rev-list --left-right --count @..."origin/${default}")
 default_a=$(echo "$default_ab" | awk '{print $1}')
 default_b=$(echo "$default_ab" | awk '{print $2}')
 
-git branch --merged "$default" | xargs printf '%s\n' | rg -q "^${current}\$" || error_code=$?
+git branch --merged "origin/${default}" | xargs printf '%s\n' | rg -q "^${current}\$" || error_code=$?
 error_code="${error_code:-0}"
 if [ "${error_code}" -eq 0 ]; then
-    merged_msg="$current is merged into $default"
+    merged_msg="$current is merged into origin/${default}"
 elif [ "${error_code}" -eq 1 ]; then
-    merged_msg="$current is not merged into $default"
+    merged_msg="$current is not merged into origin${default}"
 else
     merged_msg="Could not check if branch is merged, exit code $error_code"
 fi
 
-printf "\e[1m%s\e[0m: %s ahead, %s behind (%s)\n" "$default" "$default_a" "$default_b" "$merged_msg"
+printf "\e[1m%s\e[0m: %s ahead, %s behind (%s)\n" "origin/${default}" "$default_a" "$default_b" "$merged_msg"
 
 push_ab=$(git rev-list --left-right --count '@...@{push}')
 push_a=$(echo "$push_ab" | awk '{print $1}')
